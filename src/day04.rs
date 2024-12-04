dcfde2bb5792a7ffdeb4c25586a4c8ab13ffac1f
Attila Tajti <attila.tajti@gmail.com>
2024-12-04 13:48:58 +0100

    day 4 star 2

--- src/day04.rs
+++ src/day04.rs
@@ -1,15 +1,13 @@
 use crate::grid::{CellP, Grid};
-use anyhow::Result;
 
 pub fn run(input: &str) -> anyhow::Result<String> {
-    let s1 = star1(input)?;
-    let s2 = "";
+    let grid = Grid::parse(input)?;
+    let s1 = star1(&grid);
+    let s2 = star2(&grid);
     Ok(format!("{s1} {s2}"))
 }
 
-fn star1(input: &str) -> Result<usize> {
-    let grid = Grid::parse(input)?;
-
+fn star1(grid: &Grid<u8>) -> usize {
     let dirs = [
         (-1, -1),
         (0, -1),
@@ -21,11 +19,14 @@ fn star1(input: &str) -> Result<usize> {
         (-1, 0),
     ];
 
-    Ok(grid
-        .positions()
+    grid.positions()
         .flat_map(|p| dirs.map(|d| (p, d)))
         .filter(|(p, d)| is_grid_string(&grid, *p, *d, "XMAS"))
-        .count())
+        .count()
+}
+
+fn star2(grid: &Grid<u8>) -> usize {
+    grid.positions().filter(|p| is_x_mas(grid, *p)).count()
 }
 
 fn grid_dir_bytes(grid: &Grid<u8>, p: CellP, dir: CellP) -> impl Iterator<Item = u8> + use<'_> {
@@ -43,4 +44,15 @@ fn is_grid_string(grid: &Grid<u8>, p: CellP, dir: CellP, s: &str) -> bool {
         .filter(|(x, y)| x == y)
         .count()
         == s.len()
+}
+
+fn is_x_mas(grid: &Grid<u8>, p: CellP) -> bool {
+    if grid.get(p) != Some(&b'A') {
+        return false;
+    }
+
+    let d1 = (grid.get((p.0 - 1, p.1 - 1)), grid.get((p.0 + 1, p.1 + 1)));
+    let d2 = (grid.get((p.0 + 1, p.1 - 1)), grid.get((p.0 - 1, p.1 + 1)));
+    (d1 == (Some(&b'M'), Some(&b'S')) || d1 == (Some(&b'S'), Some(&b'M')))
+        && (d2 == (Some(&b'M'), Some(&b'S')) || d2 == (Some(&b'S'), Some(&b'M')))
 }
