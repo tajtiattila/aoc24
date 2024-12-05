00516db92db673d713f11097885879e5059a5aac
Attila Tajti <attila.tajti@gmail.com>
2024-12-05 10:20:03 +0100

    day 5 star 2

--- src/day05.rs
+++ src/day05.rs
@@ -1,19 +1,19 @@
 use anyhow::{anyhow, Result};
+use std::cmp::Ordering;
 
 pub fn run(input: &str) -> Result<String> {
     let pj = parse_print_job(input)?;
-    let s1 = star1(&pj);
-    let s2 = "";
+    let (s1, s2) = stars(&pj);
     Ok(format!("{s1} {s2}"))
 }
 
 struct PrintJob {
-    ord: Vec<(i32,i32)>,
+    ord: Vec<(i32, i32)>,
     upd: Vec<Vec<i32>>,
 }
 
 fn parse_print_job(input: &str) -> Result<PrintJob> {
-    let mut pj = PrintJob{
+    let mut pj = PrintJob {
         ord: vec![],
         upd: vec![],
     };
@@ -25,29 +25,66 @@ fn parse_print_job(input: &str) -> Result<PrintJob> {
         if line.is_empty() {
             break;
         }
-        let (x, y) = line.split_once('|').ok_or_else(|| anyhow!("invalid line"))?;
-        pj.ord.push((x.parse()?,y.parse()?));
+        let (x, y) = line
+            .split_once('|')
+            .ok_or_else(|| anyhow!("invalid line"))?;
+        pj.ord.push((x.parse()?, y.parse()?));
     }
 
     while let Some(line) = it.next() {
-        pj.upd.push(line.trim().split(',').map(|x| x.parse::<i32>()).collect::<Result<Vec<_>, _>>()?);
+        pj.upd.push(
+            line.trim()
+                .split(',')
+                .map(|x| x.parse::<i32>())
+                .collect::<Result<Vec<_>, _>>()?,
+        );
     }
 
     Ok(pj)
 }
 
-fn star1(pj: &PrintJob) -> usize {
+fn stars(pj: &PrintJob) -> (usize, usize) {
     let mut pages_after = vec![0u128; 100];
     for &(x, y) in pj.ord.iter() {
-        pages_after[x as usize] |= 1u128<<y;
+        pages_after[x as usize] |= 1u128 << y;
     }
 
-    pj.upd.iter().filter_map(|pr| {
-        let order_ok = pr.iter().scan(0u128, |state, n| {
-            let pred = *state;
-            *state |= 1u128<<n;
-            Some((pred, n))
-        }).all(|(preds, n)| pages_after[*n as usize] & preds == 0);
-        order_ok.then_some(pr[pr.len()/2 as usize] as usize)
-    }).sum()
+    pj.upd
+        .iter()
+        .map(|pr| {
+            let order_ok = pr
+                .iter()
+                .scan(0u128, |state, n| {
+                    let pred = *state;
+                    *state |= 1u128 << n;
+                    Some((pred, n))
+                })
+                .all(|(preds, n)| pages_after[*n as usize] & preds == 0);
+
+            if order_ok {
+                return (true, pr[pr.len() / 2] as usize);
+            }
+            let mut v = pr.clone();
+            v.sort_by(|&x, &y| {
+                let x_less = pages_after[x as usize] & (1 << y) == 0;
+                let y_less = pages_after[y as usize] & (1 << x) == 0;
+                match (x_less, y_less) {
+                    (false, false) => Ordering::Equal,
+                    (true, false) => Ordering::Less,
+                    (false, true) => Ordering::Greater,
+                    (true, true) => {
+                        panic!("invalid odering");
+                    }
+                }
+            });
+            (false, v[v.len() / 2] as usize)
+        })
+        .fold((0, 0), |mut acc, (is_ok, v)| {
+            if is_ok {
+                acc.0 += v;
+            } else {
+                acc.1 += v;
+            }
+            acc
+        })
 }
