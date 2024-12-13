4001cd4920b208f0309d9b5b220b24b087c40f98
Attila Tajti <attila.tajti@gmail.com>
2024-12-13 19:13:16 +0100

    day 13 star 2

--- src/day13.rs
+++ src/day13.rs
@@ -1,15 +1,19 @@
 use crate::grid::Point;
-use std::iter::successors;
 use anyhow::{anyhow, bail, Result};
 
 pub fn run(input: &str) -> Result<String> {
     let machines = parse_machines(input)?;
-    let s1 = min_tokens(&machines);
-    let s2 = "";
+    for m in &machines {
+        if m.a.x * m.b.y == m.a.y * m.b.x {
+            println!("{m:?} is colinear");
+        }
+    }
+    let s1 = min_tokens_shift(&machines, 0);
+    let s2 = min_tokens_shift(&machines, STAR_2_SHIFT);
     Ok(format!("{s1} {s2}"))
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 struct Machine {
     a: Point,
     b: Point,
@@ -18,22 +22,45 @@ struct Machine {
 
 const A_PRICE: usize = 3;
 const B_PRICE: usize = 1;
+const STAR_2_SHIFT: i64 = 10_000_000_000_000;
 
-fn min_tokens(m: &[Machine]) -> usize {
-    m.iter().filter_map(find_min_tokens).sum()
+fn min_tokens_shift(m: &[Machine], prize_shift: i64) -> usize {
+    m.iter()
+        .filter_map(|m| find_min_tokens_shift(m, prize_shift))
+        .sum()
 }
 
-fn find_min_tokens(m: &Machine) -> Option<usize> {
-    successors(Some(Point::new(0, 0)), |p| (p.x < m.prize.x && p.y < m.prize.y).then_some(*p + m.a))
-        .enumerate()
-        .filter_map(|(ma, ama)| {
-            let d = m.prize - ama;
-            let mbx = d.x / m.b.x;
-            let mby = d.y / m.b.y;
-            (mbx == mby && d.x % m.b.x == 0 && d.y % m.b.y == 0).then_some((ma, mbx as usize))
-        })
-        .map(|(ma, mb)| A_PRICE*ma + B_PRICE*mb)
-        .min()
+fn find_min_tokens_shift(machine: &Machine, prize_shift: i64) -> Option<usize> {
+    let ax = machine.a.x as f64;
+    let ay = machine.a.y as f64;
+    let bx = machine.b.x as f64;
+    let by = machine.b.y as f64;
+    let px = machine.prize.x as f64 + prize_shift as f64;
+    let py = machine.prize.y as f64 + prize_shift as f64;
+
+    let m = if by.abs() > bx.abs() {
+        (px - py * bx / by) / (ax - ay * bx / by)
+    } else {
+        (py - px * by / bx) / (ay - ax * by / bx)
+    }
+    .round() as i64;
+
+    let n = if ay.abs() > bx.abs() {
+        (px - py * ax / ay) / (bx - by * ax / ay)
+    } else {
+        (py - px * ay / ax) / (by - bx * ay / ax)
+    }
+    .round() as i64;
+
+    let ax = machine.a.x as i64;
+    let ay = machine.a.y as i64;
+    let bx = machine.b.x as i64;
+    let by = machine.b.y as i64;
+    let px = machine.prize.x as i64 + prize_shift;
+    let py = machine.prize.y as i64 + prize_shift;
+
+    let (qx, qy) = (m * ax + n * bx, m * ay + n * by);
+    ((px, py) == (qx, qy)).then(|| m as usize * A_PRICE + n as usize * B_PRICE)
 }
 
 fn parse_machines(input: &str) -> Result<Vec<Machine>> {
@@ -43,11 +70,15 @@ fn parse_machines(input: &str) -> Result<Vec<Machine>> {
         let a = parse_def(Some(line), "Button A")?;
         let b = parse_def(lines.next(), "Button B")?;
         let prize = parse_def(lines.next(), "Prize")?;
-        r.push(Machine{a, b, prize });
+        r.push(Machine { a, b, prize });
         match lines.next() {
-            Some("") => {},
-            None => { break; },
-            Some(x) => { bail!("expected empty line, got {x}"); },
+            Some("") => {}
+            None => {
+                break;
+            }
+            Some(x) => {
+                bail!("expected empty line, got {x}");
+            }
         }
     }
     Ok(r)
@@ -55,12 +86,16 @@ fn parse_machines(input: &str) -> Result<Vec<Machine>> {
 
 fn parse_def(line: Option<&str>, prefix: &str) -> Result<Point> {
     let line = line.ok_or_else(|| anyhow!("unexpected eof"))?;
-    let (ps, zs) = line.split_at_checked(prefix.len()).ok_or_else(|| anyhow!("expected {prefix} in {line}"))?;
+    let (ps, zs) = line
+        .split_at_checked(prefix.len())
+        .ok_or_else(|| anyhow!("expected {prefix} in {line}"))?;
     if ps != prefix {
         bail!(anyhow!("expected {prefix} in {line}"));
     }
     let zs = zs.trim_start_matches(':').trim();
-    let (xs, ys) = zs.split_once(',').ok_or_else(|| anyhow!("invalid line {line}"))?;
+    let (xs, ys) = zs
+        .split_once(',')
+        .ok_or_else(|| anyhow!("invalid line {line}"))?;
     Ok(Point::new(parse_num(xs, 'X')?, parse_num(ys, 'Y')?))
 }
 
@@ -91,8 +126,15 @@ Prize: X=7870, Y=6450
 Button A: X+69, Y+23
 Button B: X+27, Y+71
 Prize: X=18641, Y=10279
-"#.trim();
+"#
+        .trim();
         let m = parse_machines(input).unwrap();
-        assert_eq!(min_tokens(&m), 480);
+        assert_eq!(m.len(), 4);
+        assert_eq!(min_tokens_shift(&m, 0), 480);
+
+        assert!(find_min_tokens_shift(&m[0], STAR_2_SHIFT).is_none());
+        assert!(find_min_tokens_shift(&m[1], STAR_2_SHIFT).is_some());
+        assert!(find_min_tokens_shift(&m[2], STAR_2_SHIFT).is_none());
+        assert!(find_min_tokens_shift(&m[3], STAR_2_SHIFT).is_some());
     }
 }
