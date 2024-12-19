6107b119957ece23a39ba5bddf9b5bc71abe1673
Attila Tajti <attila.tajti@gmail.com>
2024-12-19 11:47:41 +0100

    day 19 star 2

--- src/day19.rs
+++ src/day19.rs
@@ -1,51 +1,87 @@
 use anyhow::{anyhow, Result};
-use std::collections::{BTreeMap, BTreeSet, HashSet};
+use std::collections::{BTreeMap, HashSet};
 
 pub fn run(input: &str) -> Result<String> {
-    let s1 = star1(input)?;
-    let s2 = "";
+    let (s1, s2) = stars(input)?;
     Ok(format!("{s1} {s2}"))
 }
 
 type LenToPats<'a> = BTreeMap<usize, HashSet<&'a str>>;
 
-fn star1(input: &str) -> Result<usize> {
+fn stars(input: &str) -> Result<(usize, usize)> {
     let mut it = input.lines();
 
     let ltp = len_to_pats(it.next().ok_or_else(|| anyhow!("invalid input"))?);
 
     it.next().ok_or_else(|| anyhow!("invalid input"))?;
 
-    Ok(it.filter(|design| design_possible(design, &ltp)).count())
+    Ok(it.fold((0, 0), |(nok, narr), design| {
+        let n = arrangements(design, &ltp);
+        (nok + (n > 0) as usize, narr + n)
+    }))
 }
 
 fn len_to_pats(line: &str) -> LenToPats {
     line.split(", ").fold(LenToPats::new(), |mut m, pat| {
-            m.entry(pat.len())
-                .and_modify(|s| {
-                    s.insert(pat);
-                })
-                .or_insert(HashSet::from([pat]));
-            m
+        m.entry(pat.len())
+            .and_modify(|s| {
+                s.insert(pat);
+            })
+            .or_insert(HashSet::from([pat]));
+        m
     })
 }
 
-fn design_possible(design: &str, ltp: &LenToPats) -> bool {
-    let mut vis = BTreeSet::from([0]);
-    let mut poss = vec![0];
-    while let Some(p) = poss.pop() {
+fn arrangements(design: &str, ltp: &LenToPats) -> usize {
+    let mut counts = vec![0; design.len() + 1];
+    counts[0] = 1;
+    for p in 0..design.len() {
+        let nfrom = counts[p];
+        if nfrom == 0 {
+            continue;
+        }
         let work = &design[p..];
         for (&l, pats) in ltp.iter().take_while(|(l, _)| **l <= work.len()) {
             let cur = &work[0..l];
             if pats.contains(cur) {
                 let q = p + l;
-                if q == design.len() {
-                    return true;
-                } else if vis.insert(q) {
-                    poss.push(q);
-                }
+                counts[q] += nfrom;
             }
         }
     }
-    false
+    counts[design.len()]
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let sample = r#"
+r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb
+"#
+        .trim();
+        let ltp = len_to_pats(sample.split_once('\n').unwrap().0);
+
+        assert_eq!(arrangements("brwrr", &ltp), 2);
+        assert_eq!(arrangements("bggr", &ltp), 1);
+        assert_eq!(arrangements("gbbr", &ltp), 4);
+        assert_eq!(arrangements("rrbgbr", &ltp), 6);
+        assert_eq!(arrangements("bwurrg", &ltp), 1);
+        assert_eq!(arrangements("brgr", &ltp), 2);
+        assert_eq!(arrangements("ubwu", &ltp), 0);
+        assert_eq!(arrangements("bbrgwb", &ltp), 0);
+
+        assert_eq!(stars(sample).unwrap(), (6, 16));
+    }
 }
