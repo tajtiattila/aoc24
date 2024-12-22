bbd74c55d67828e265252675c9f066541848f7c6
Attila Tajti <attila.tajti@gmail.com>
2024-12-22 10:53:02 +0100

    day 21 star 2

--- src/day21.rs
+++ src/day21.rs
@@ -1,10 +1,9 @@
-use std::collections::{HashSet, VecDeque};
+use std::collections::{HashMap, HashSet, VecDeque};
 use std::rc::Rc;
 
 pub fn run(input: &str) -> anyhow::Result<String> {
-    sim_rec("379A", 8);
-    let s1 = star1(input);
-    let s2 = "";
+    let s1 = star(1, input);
+    let s2 = star(2, input);
     Ok(format!("{s1} {s2}"))
 }
 
@@ -14,6 +13,24 @@ fn star1(input: &str) -> usize {
         .filter_map(|line| {
             let n: usize = line.trim_end_matches('A').parse().ok()?;
             let m = star1_moves(line);
+            Some(n * m)
+        })
+        .sum()
+}
+
+fn star(num: usize, input: &str) -> usize {
+    let km = match num {
+        1 => analyze(DIRPAD, 0),
+        2 => analyze(DIRPAD, 23),
+        _ => panic!("invalid star number")
+    }.expect("dirpad analysis failed");
+
+    input
+        .lines()
+        .filter_map(|line| {
+            let n: usize = line.trim_end_matches('A').parse().ok()?;
+            let d = dirpad_moves(numpad_moves(line)).collect::<String>();
+            let m = km.input_count(&d)?;
             Some(n * m)
         })
         .sum()
@@ -166,25 +183,33 @@ fn dirpad_moves(input: impl Iterator<Item = char>) -> impl Iterator<Item = char>
     keypad_moves(DIRPAD, input)
 }
 
-fn keypad_moves<I: Iterator<Item = char>>(keypad: &str, input: I) -> impl Iterator<Item = char> + use<'_, I> {
+fn keypad_moves<I: Iterator<Item = char>>(
+    keypad: &str,
+    input: I,
+) -> impl Iterator<Item = char> + use<'_, I> {
     let p = keypad.find('A').unwrap();
     input
         .scan(p, move |state, c| {
-            let p = *state;
-            let q = keypad.find(c)?;
+            let (q, dx, dy, x_first) = keypad_move(keypad, *state, c)?;
             *state = q;
-            let (px, py) = ((p % 4) as i32, (p / 4) as i32);
-            let (qx, qy) = ((q % 4) as i32, (q / 4) as i32);
-            let dx = qx - px;
-            let dy = qy - py;
-            // char under arm when moving x first
-            let qxc = keypad.as_bytes()[(qx + py * 4) as usize];
-            // char under arm when moving y first
-            let qyc = keypad.as_bytes()[(px + qy * 4) as usize];
-            let x_first = (dx < 0 && qxc != b'.') || qyc == b'.';
             Some(movement_chars(dx, dy, x_first))
         })
         .flatten()
+}
+
+fn keypad_move(keypad: &str, from_pos: usize, to_key: char) -> Option<(usize, i32, i32, bool)> {
+    let p = from_pos;
+    let q = keypad.find(to_key)?;
+    let (px, py) = ((p % 4) as i32, (p / 4) as i32);
+    let (qx, qy) = ((q % 4) as i32, (q / 4) as i32);
+    let dx = qx - px;
+    let dy = qy - py;
+    // char under arm when moving x first
+    let qxc = keypad.as_bytes()[(qx + py * 4) as usize];
+    // char under arm when moving y first
+    let qyc = keypad.as_bytes()[(px + qy * 4) as usize];
+    let x_first = (dx < 0 && qxc != b'.') || qyc == b'.';
+    Some((q, dx, dy, x_first))
 }
 
 fn movement_chars(x: i32, y: i32, x_first: bool) -> impl Iterator<Item = char> {
@@ -213,7 +238,7 @@ fn star1_moves(digits: &str) -> usize {
         .count()
 }
 
-fn sim_rec(digits: &str, n: usize) {
+fn print_sim_rec(digits: &str, n: usize) {
     let mut s = String::from(digits);
     println!("0: {}", s);
     let mut keypad = NUMPAD;
@@ -221,26 +246,98 @@ fn sim_rec(digits: &str, n: usize) {
         let next = keypad_moves(keypad, s.chars()).collect();
         keypad = DIRPAD;
         s = next;
-        println!("{}: {}", i+1, s);
+        println!("{}: {}", i + 1, s);
     }
     println!();
 }
 
+fn print_sim_dir(input: &str, n: usize) {
+    let mut s = String::from(input);
+    println!("0: {}", s);
+    for i in 0..n {
+        let next = dirpad_moves(s.chars()).collect();
+        s = next;
+        println!("{}: {}", i + 1, s);
+    }
+    println!();
+}
+
+fn sim_dir(input: &str, depth: usize) -> String {
+    let mut s = String::from(input);
+    for _ in 0..depth {
+        let next = dirpad_moves(s.chars()).collect();
+        s = next;
+    }
+    s
+}
+
+fn move_sets(keypad: &str) -> Vec<String> {
+    let mut v = vec![String::from('A')];
+    for (p, cp) in keypad.chars().enumerate().filter(|(_, c)| *c != '.') {
+        for cq in keypad.chars().filter(|c| *c != '.' && *c != cp) {
+            let (_, dx, dy, x_first) = keypad_move(keypad, p, cq).unwrap();
+            v.push(movement_chars(dx, dy, x_first).collect());
+        }
+    }
+    v
+}
+
+#[derive(Debug, Clone)]
+struct KeypadMap(HashMap<String, usize>);
+
+impl KeypadMap {
+    fn from_keypad(keypad: &str) -> Self {
+        Self(
+            move_sets(keypad)
+                .into_iter()
+                .map(|s| {
+                    let n = keypad_moves(keypad, s.chars()).count();
+                    (s, n)
+                })
+                .collect(),
+        )
+    }
+
+    fn input_count(&self, input: &str) -> Option<usize> {
+        input.split_inclusive('A').fold(Some(0), |acc, part| {
+            acc.and_then(|sum| Some(sum + self.0.get(part)?))
+        })
+    }
+
+    fn next_level(&self, keypad: &str) -> Option<Self> {
+        Some(Self(
+            self.0
+                .keys()
+                .map(|s0| {
+                    let s1 = keypad_moves(keypad, s0.chars()).collect::<String>();
+                    self.input_count(&s1).map(|n1| (s0.clone(), n1))
+                })
+                .collect::<Option<HashMap<String, usize>>>()?,
+        ))
+    }
+}
+
+// vA
+// <vA^>A
+fn analyze(keypad: &str, depth: usize) -> Option<KeypadMap> {
+    let mut m = KeypadMap::from_keypad(keypad);
+    for _ in 0..depth {
+        m = m.next_level(keypad)?;
+    }
+    Some(m)
+}
+
 // https://docs.rs/apply/latest/src/apply/lib.rs.html
 pub trait Apply<Res> {
-    fn apply<F: FnOnce(Self) -> Res>(self, f: F) -> Res where Self: Sized {
+    fn apply<F: FnOnce(Self) -> Res>(self, f: F) -> Res
+    where
+        Self: Sized,
+    {
         f(self)
     }
 }
 
-impl<T: ?Sized, Res> Apply<Res> for T { }
-
-fn star1_entry(digits: &str) -> usize {
-    let s0 = numpad_entry(digits);
-    let s1 = dirpad_entry(&s0);
-    let s2 = dirpad_entry(&s1);
-    s2.len()
-}
+impl<T: ?Sized, Res> Apply<Res> for T {}
 
 fn keypad_entry(keypad: &str, digits: &str) -> String {
     let mut out = String::new();
@@ -422,6 +519,28 @@ mod test {
         .trim();
 
         assert_eq!(star1(sample), 126384);
+
+        let sim_eq_from_parts = |a, b, n| -> bool {
+            let l = sim_dir(a, n) + &sim_dir(b, n);
+            let r = sim_dir(&format!("{a}{b}"), n);
+            l == r
+        };
+        assert!(sim_eq_from_parts("<<A", "^^A", 8));
+
+        let m1 = analyze(DIRPAD, 0).unwrap();
+        println!("{m1:?}");
+
+        let check1 = |digits| {
+            let d = dirpad_moves(numpad_moves(digits)).collect::<String>();
+            println!("{d}");
+            m1.input_count(&d)
+        };
+        assert_eq!(check1("029A"), Some(68));
+        assert_eq!(check1("980A"), Some(60));
+        assert_eq!(check1("179A"), Some(68));
+        assert_eq!(check1("456A"), Some(64));
+        assert_eq!(check1("379A"), Some(64));
+
         //----------------------------------------------------------------
 
         let s0 = numpad_entry("029A");
@@ -452,8 +571,10 @@ mod test {
         let s1 = dirpad_entry(&s0);
         assert_eq!(&s1, "<A>A<AAv<AA>>^AvAA^Av<AAA>^A");
         let s2 = dirpad_entry(&s1);
-        //  <A>A<AAv<AA >>^AvAA^Av<AAA>^A
-        assert_eq!(&s2, "v<<A>>^AvA^Av<<A>>^AAv<A<A>>^AA");
-        // v<<A>>^AvA^Av<<A>>^AAv<A<A>>^AAvAA^<A>Av<A>^AA<A>Av<A<A>>^AAAvA^<A>A
+        assert_eq!(
+            &s2,
+            "v<<A>>^AvA^Av<<A>>^AAv<A<A>>^AAvAA^<A>Av<A>^AA<A>Av<A<A>>^AAAvA^<A>A"
+        );
+        //
     }
 }
